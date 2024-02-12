7ab1034aab8535902cd658ff20a3ccb5ab982bb0
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2024-02-12 09:23:00 -0500

    fg

--- vue-project/src/components/DestCard.vue
+++ vue-project/src/components/DestCard.vue
@@ -1,22 +1,26 @@
 <template>
     <div>
-        <h1>{{ Target.name }}</h1>
-        <img :src="Target.img" alt="">
-        <h2>{{ clicked }}</h2>
-        <button @click="increment">Click ME</button>
+     <div class="flexcontainer">
+        <div id="card">
+         <h1>{{ Target.name }}</h1>
+         <img :src="Target.img" alt="">
+<!--          <h2>{{ clicked }}</h2> -->
+<!--         <button @click="increment">Click ME</button> -->
+        </div>
+     </div>  
     </div>
 </template>
 
 <script setup>
-import {ref} from "vue";
-const props = defineProps({
-    Target: Object, 
-});
+/* import {ref} from "vue";
 //clicker logic
 const clicked = ref(0);
 function increment(){
     clicked.value++;
-}
+}  */
+const props = defineProps({
+    Target: Object, 
+});
 </script>
 
 <style scoped>
@@ -25,4 +29,25 @@ img {
     height:300px;
     object-fit:cover;
 }
+.flexcontainer {
+    flex-wrap: wrap;
+    display: flex;
+    justify-content: space-around;
+}
+#card {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    background-color: rgba(110, 110, 241, 0.774);
+    width: 15vw;
+    height: 43vh;
+    font-size: 1rem;
+    border-radius: 15px;
+    padding-top:3vh;
+    padding-bottom: 3vh;
+    margin: 7.5vh;
+    box-shadow: 10px 10px;
+   }
 </style>
--- vue-project/src/views/AssassinationContract.vue
+++ vue-project/src/views/AssassinationContract.vue
@@ -1,35 +1,9 @@
 <template>
   <div>
-    <DestCard v-for="target in targets" 
-    :key="target.name" 
-    :Target="target"/>
   </div>
 </template>
 
 <script setup>
-import DestCard from "@/components/DestCard.vue"
-const targets = [
-  { name: "Templar Grand Master Robert de Sablé", image: "image1.jpg" },
-  { name: "Al Mualim", image: "image2.jpg" },
-  { name: "William of Montferrat", image: "image3.jpg" },
-  { name: "Abu'l Nuqoud", image: "image4.jpg" },
-  { name: "Majd Addin", image: "image5.jpg" },
-  { name: "Jubair al Hakim", image: "image6.jpg" },
-  { name: "Sibrand", image: "image7.jpg" },
-  { name: "Robert de Sable", image: "image8.jpg" },
-  { name: "Tamir", image: "image9.jpg" },
-  { name: "Garnier de Naplouse", image: "image10.jpg" },
-  { name: "Talal", image: "image11.jpg" },
-  { name: "Abbas Sofian", image: "image12.jpg" },
-  { name: "Moloch", image: "image13.jpg" },
-  { name: "Shahar", image: "image14.jpg" },
-  { name: "Shareen", image: "image15.jpg" },
-  { name: "Kadar", image: "image16.jpg" },
-  { name: "Tamir", image: "image17.jpg" },
-  { name: "Sibrand", image: "image18.jpg" },
-  { name: "Talal", image: "image19.jpg" },
-  { name: "Abu'l Nuqoud", image: "image20.jpg" }
-];
 </script>
 
 <style scoped>
