e7505ebb0c088ae3608c49c2d839a95783a568f5
DannyL1234 <144807703+DanielL492@users.noreply.github.com>
2024-02-21 15:20:01 -0500

    Revert "ssd"
    This reverts commit 38e0ec300e76b63703548f6f2deb8f75729411cb.

---
+++ vue-project/src/components/TargetCard.vue
@@ -0,0 +1,47 @@
+<template>
+        <div id={{ Target.name }}>
+         <h1>{{ Target.name }}</h1>
+         <img :src="Target.img" alt="" class="card-img">
+         <h2>{{ clicked }}</h2> 
+         <button @click="increment">Assassinate</button> 
+        </div> 
+</template>
+
+<script setup>
+//import {ref} from "vue";
+let dead = [];
+//clicker logic
+//const clicked = ref(0);
+function increment(){
+    dead.push();
+}  
+const props = defineProps({
+    Target: Object, 
+});
+</script>
+
+<style scoped>
+.card-img {
+    width:200px;
+    height:250px;
+    align-items: center;
+}
+
+#card {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    background-color: rgba(110, 110, 241, 0.774);
+    width: 22.5vw;
+    height: 45vh;
+    font-size: 1rem;
+    border-radius: 15px;
+    padding-top:3vh;
+    padding-bottom: 3vh;
+    margin: 7.5vh;
+    box-shadow: 10px 10px;
+    text-align: center;
+   }
+</style>
