c7334d0680d8341245857339aa35c45d925bf79b
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2024-02-29 09:22:27 -0500

    sd

--- vue-project/src/stores/counter.js
+++ vue-project/src/stores/counter.js
@@ -1,12 +1,33 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref } from "vue";
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+const targets = [
+  { name: "Templar Grand Master Robert de Sablé", 
+    image: "image1.jpg", dead: ref(false),
+    dead: ref(false) },
+  { name: "Al Mualim", 
+    image: "image2.jpg", dead: ref(false) },
+  { name: "William of Montferrat", image: "image3.jpg", dead: ref(false),
+  dead: ref(false) },
+  { name: "Abu'l Nuqoud", image: "image4.jpg", dead: ref(false),
+  dead: ref(false) },
+  { name: "Majd Addin", image: "image5.jpg", dead: ref(false),
+  dead: ref(false) },
+  { name: "Jubair al Hakim", image: "image6.jpg", dead: ref(false),
+  dead: ref(false) },
+  { name: "Sibrand", image: "image7.jpg", dead: ref(false),
+  dead: ref(false) },
+  { name: "Robert de Sable", image: "image8.jpg", dead: ref(false) },
+  { name: "Tamir", image: "image9.jpg", dead: ref(false) },
+  { name: "Garnier de Naplouse", image: "image10.jpg", dead: ref(false) },
+  { name: "Talal", image: "image11.jpg", dead: ref(false) },
+  { name: "Abbas Sofian", image: "image12.jpg", dead: ref(false) },
+  { name: "Moloch", image: "image13.jpg", dead: ref(false) },
+  { name: "Shahar", image: "image14.jpg", dead: ref(false) },
+  { name: "Shareen", image: "image15.jpg", dead: ref(false) },
+  { name: "Kadar", image: "image16.jpg", dead: ref(false) },
+  { name: "Tamir", image: "image17.jpg", dead: ref(false) },
+  { name: "Sibrand", image: "image18.jpg", dead: ref(false) },
+  { name: "Abu'l Nuqoud", image: "image20.jpg", dead: ref(false) }
+];
 
-  return { count, doubleCount, increment }
-})
+export { targets }
--- vue-project/src/views/AssassinationContract.vue
+++ vue-project/src/views/AssassinationContract.vue
@@ -11,27 +11,8 @@
 
 <script setup>
 import TargetCard from "@/components/TargetCard.vue"
-const targets = [
-  { name: "Templar Grand Master Robert de Sablé", image: "image1.jpg" },
-  { name: "Al Mualim", image: "image2.jpg" },
-  { name: "William of Montferrat", image: "image3.jpg" },
-  { name: "Abu'l Nuqoud", image: "image4.jpg" },
-  { name: "Majd Addin", image: "image5.jpg" },
-  { name: "Jubair al Hakim", image: "image6.jpg" },
-  { name: "Sibrand", image: "image7.jpg" },
-  { name: "Robert de Sable", image: "image8.jpg" },
-  { name: "Tamir", image: "image9.jpg" },
-  { name: "Garnier de Naplouse", image: "image10.jpg" },
-  { name: "Talal", image: "image11.jpg" },
-  { name: "Abbas Sofian", image: "image12.jpg" },
-  { name: "Moloch", image: "image13.jpg" },
-  { name: "Shahar", image: "image14.jpg" },
-  { name: "Shareen", image: "image15.jpg" },
-  { name: "Kadar", image: "image16.jpg" },
-  { name: "Tamir", image: "image17.jpg" },
-  { name: "Sibrand", image: "image18.jpg" },
-  { name: "Abu'l Nuqoud", image: "image20.jpg" }
-];
+import { targets } from "@/stores/counter.js";
+
 
 
 /*function insertimg(arr) {
