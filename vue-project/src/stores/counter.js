acc8f262517e94a8de5dc897e2135a9a9d31577e
DanielL492 <lindan18351@gmail.com>
2024-03-06 09:15:06 -0500

    Revert "sd"
    This reverts commit 6041d9e8ad439014a9cb126d5481e77707a71c12.

---
+++ vue-project/src/stores/counter.js
@@ -0,0 +1,30 @@
+import { ref } from "vue";
+import { reactive } from "vue";
+
+export const targets = reactive([
+  { name: "Robert de Sablé", 
+    img: "/img/Robert.jpeg", dead:  false, num: 0, },
+  { name: "Al Mualim", 
+    img: "/img/Mualim.jpeg", dead:  false, num: 1, },
+  { name: "William of Montferrat", img: "/img/William.jpeg",
+  dead:  false, num: 2, },
+  { name: "Abu'l Nuqoud", img: "/img/Nuqoud.jpeg",
+  dead:  false, num: 3, },
+  { name: "Majd Addin", img: "/img/addin.jpeg",
+  dead:  false, num: 4, },
+  { name: "Jubair al Hakim", img: "/img/Jubair.jpeg",
+  dead:  false, num: 5, },
+  { name: "Sibrand", img: "/img/Sibrand.jpeg", 
+  dead:  false, num: 6, },
+  { name: "Tamir", img: "/img/Tamir.jpeg", dead:  false, num: 7, },
+  { name: "Garnier de Naplouse", img: "/img/Naplouse.jpeg", dead:  false, num: 8, },
+  { name: "Talal", img: "/img/Talal.jpeg", dead:  false, num: 9, },
+  { name: "Abbas Sofian", img: "/img/Abbas.jpeg", dead:  false, num: 10, },
+  { name: "Moloch", img: "/img/Moloch.jpeg", dead:  false, num: 11, },
+  { name: "Shahar", img: "/img/Shahar.jpeg", dead:  false, num: 12, },
+  { name: "Kadar", img: "/img/Kadar.jpeg", dead:  false, num: 13, },
+]);
+
+export let deadlist = reactive([])
+
+export let history = reactive([])
---
+++ vue-project/src/views/TargetList.vue
@@ -0,0 +1,32 @@
+<template>
+      <div class="flexcontainer">
+            <TargetCard v-for="target in targets"
+            :Target="target"/>
+      </div>
+</template>
+
+
+<script setup>
+import TargetCard from "@/components/TargetCard.vue"
+import { targets } from "@/stores/counter.js";
+
+
+
+
+</script>
+
+
+<style scoped>
+
+.flexcontainer {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+}
+
+h1 {
+  color: red;
+}
+
+</style>
